0a8b18601d97317eace3f23f7499bcfe8bb85c34
Aadarsh Jain <aadarshjain0605@gmail.com>
2024-03-17 10:25:00 +0000

    CCBP Timeline Solution

---
+++ src/components/ProjectTimelineCard/index.js
@@ -0,0 +1,33 @@
+import {AiFillCalendar} from 'react-icons/ai'
+
+import './index.css'
+
+const ProjectTimelineCard = props => {
+  const {projectDetails} = props
+  const {
+    imageUrl,
+    projectTitle,
+    description,
+    projectUrl,
+    duration,
+  } = projectDetails
+
+  return (
+    <div className="project-card-container">
+      <img src={imageUrl} alt="project" className="project-image" />
+      <div className="project-title-and-duration-container">
+        <h1 className="project-title">{projectTitle}</h1>
+        <div className="duration-container">
+          <AiFillCalendar className="calendar-icon" />
+          <p className="duration">{duration}</p>
+        </div>
+      </div>
+      <p className="description">{description}</p>
+      <a href={projectUrl} className="visit-link">
+        Visit
+      </a>
+    </div>
+  )
+}
+
+export default ProjectTimelineCard
